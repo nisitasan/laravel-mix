04bd1a94b926533703f135c50dd547571fee21bd
aaa <aaa>
2020-02-11 20:27:04 +0800

    Init

---
+++ src/app.js
@@ -0,0 +1,21 @@
+//透過require引入初始化檔案
+require('./bootstrap')
+
+import StudentNameList from './componets/StudentNameList'
+import AddStudent from './componets/AddStudent'
+
+new Vue({
+    el: '#app',
+    components: { StudentNameList, AddStudent },
+    data() {
+        return {
+            title: 'Student List',
+            
+        }
+    },
+    methods: {
+        doAdd(name) {
+            this.$refs.table.doAdd(name);
+        }
+    }
+})
